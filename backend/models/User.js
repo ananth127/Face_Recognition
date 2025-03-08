26c32a7b80804215931625a4ef8f8a340b630c58
Ananth S <138275866+ananth127@users.noreply.github.com>
2025-03-08 10:07:04 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   faceDescriptor: { type: [Number], required: true }, // Storing descriptor as an array
+  lastActive: { type: Date, default: Date.now } // Store last login timestamp
 });
 
 const User = mongoose.model("User", userSchema);
